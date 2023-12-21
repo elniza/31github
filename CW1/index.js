0f80e9c4f5f192435f7523ea17ab0e12e487f8d7
Elniza <elnizaadylkyzy63@gmail.com>
2023-12-21 12:24:30 +0600

    day-21

---
+++ CW1/index.js
@@ -0,0 +1,64 @@
+console.log("index.js");
+var x=5;
+console.log(x);
+x = x + x;
+console.log(x);
+
+x = "banana";
+console.log(x);
+x = 'watermelon';
+console.log (x);
+x = "cherry";
+console.log (x);
+
+var n = "New";
+var y = "York";
+var z = "Zealand";
+
+var ny = n + " " + y;
+console.log(ny);// "New York"
+
+var nz = n + " " + z;
+console.log(nz);// "New Zealand"
+
+var a = "I am ";
+var b = "going through a ";
+var c = "tunnel";
+var d = "Over there is ";
+var e = "the biggest ";
+var f = "turtle";
+var h = "i am ";
+var l = " the biggest turtle";
+var abc = a + " " + b + " " +c
+console.log(abc);
+var abc = d + " " + e + " " +f
+console.log(abc);
+var abc = h + " " + l 
+console.log(abc);
+
+var n = " I am";
+var k = " a future";
+var g = " front-end developer"
+var nkg = n + " " + k + " " + g
+console.log(nkg);
+
+
+
+var name = "Elniza";
+
+var fullname = "Elniza Adylova";
+
+var number = 3.14159265359;
+
+var text = "abcdefghijklmnopqrstuvwxyz";
+
+var lengthOfSquare = 5;
+
+var squareOfArea = lengthOfSquare * lengthOfSquare
+
+var radiusOfCircle = 4;
+
+var circle = c * radiusOfCircle;
+function x() {
+    console.log('Elniza ver')
+}
